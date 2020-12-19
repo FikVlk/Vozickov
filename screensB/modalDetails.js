ec3376c04ce4d3631fe714f7bd4957af8a15975a
FikVlk <filipvlkous@gmail.com>
2020-12-19 20:31:24 +0100

    small change

--- screensB/modalDetails.js
+++ screensB/modalDetails.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { StyleSheet, Button, View } from "react-native";
+import { globalStyles } from "../Styles/globalStyles";
 
 export default function ModalDetails({ closeModal, navigation, id }) {
   return (
-    <View>
+    <View style={globalStyles.container}>
+      <Text style={styles.boldText}>Modal</Text>
       <View style={{ marginTop: 50 }}>
         <Button title="Close" onPress={closeModal} />
         <Button
@@ -20,4 +22,9 @@ export default function ModalDetails({ closeModal, navigation, id }) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  boldText: {
+    fontFamily: "Nunito_700Bold",
+    fontSize: 24,
+  },
+});
